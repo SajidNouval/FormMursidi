f0fd977e667c92b08e515a12fdefc9532ca90abe
titahsahputra <titahsahputra123@gmail.com>
2024-11-29 20:58:34 +0700

    kredit by irfan mursidi

--- Form/app/Models/Ruang_Kuliah.php
+++ Form/app/Models/Ruang_Kuliah.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Ruang_Kuliah extends Model
 {
-    //
+         /** @use HasFactory<\Database\Factories\UserFactory> */
+         use HasFactory, Notifiable;
+
+         protected $table = 'ruang_kuliah'; // Tentukan nama tabel yang benar
+         /**
+          * The attributes that are mass assignable.
+          *
+          * @var array<int, string>
+          */
+         protected $fillable = [
+            'id',
+            'ruang_kuliah_kode_ruang',
+            'mata_kuliah_kode_mk',
+            'hari',
+            'jam_mulai',
+            'jam_selesai'
+         ];
+         
+     
+         /**
+          * The attributes that should be hidden for serialization.
+          *
+          * @var array<int, string>
+        
+          * Get the attributes that should be cast.
+          *
+          * @return array<string, string>
+          */
 }
